f17966f2f128ef8dc4dabba6dd95df06311ac1e8
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-04-09 11:51:13 -0400

    .
    .

--- src/components/header.js
+++ src/components/header.js
@@ -28,6 +28,17 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
     </div>
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+    <script>
+      if (window.netlifyIdentity){" "}
+      {window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/"
+          })
+        }
+      })}
+    </script>
   </header>
 )
 
